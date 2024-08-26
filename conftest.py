76eb2fd77ad49d5fde07cafe3e184077d9929e6c
PRO1004nDRUha <karavaev01andrew@gmail.com>
2024-08-26 17:40:43 +0300

    Создан файл locators.py в котором по классам(веб-страницам) разбиты необходимые на них локаторы. В следствии работа всего проекта была переписана под локаторы. Добавлен файл login_page.py в котором описана логика работы со страницей аутентификации. А именно: проверка корректного url, проверка наличия форм логина и регистрации. В файле main_page.py логика переписана под локаторы. В файле conftest.py была исправлена ошибка с открытием браузера каждую тестовую ф-цию. Теперь, каждую сессию. Добавлен файл test_login_page.py, который тестирует файд login_page.py.

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ def pytest_addoption(parser):
     parser.addoption("--language", action="store", default="en", help="Choose language")
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session")
 def browser(request):
     language = request.config.getoption("language")
     option = Options()
---
+++ pages/login_page.py
@@ -0,0 +1,19 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert "login" in self.browser.current_url.split("/"), "substring login not found in url"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "login form not found"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "registration form not found"
